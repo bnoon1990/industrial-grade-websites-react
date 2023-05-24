c64d3790caa264c70ac825627f87e5a5d9578ab3
Ben Noon <Ben@Nooneliterecruitment.com>
2023-05-24 21:05:45 +0100

    Fixed className typo

--- src/components/Feature.jsx
+++ src/components/Feature.jsx
@@ -1,8 +1,8 @@
 function Feature(props) {
     return (
     <section className="container mx-auto px-4 py-4 md:w-1/2 rounded bg-neutral-600 my-4">
-        <h2 classNameName="text-4xl font-bold uppercase text-neutral-300a pb-2">{props.headline}</h2>
-        <p classNameName="text-neutral-300 font-mono text-lg">{props.blurb}</p>
+        <h2 className="text-4xl font-bold uppercase text-neutral-300a pb-2">{props.headline}</h2>
+        <p className="text-neutral-300 font-mono text-lg">{props.blurb}</p>
         {props.form}
     </section>
     )
