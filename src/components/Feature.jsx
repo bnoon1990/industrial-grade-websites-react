15c9f942e285e2fb5bd42beabcdd21c4eac70ce0
Ben Noon <Ben@Nooneliterecruitment.com>
2023-05-24 21:05:03 +0100

    Added basic example server to project to handle  form response. Form fields colllected and emailed to address of  choosing via ssl.

--- src/components/Feature.jsx
+++ src/components/Feature.jsx
@@ -1,8 +1,8 @@
 function Feature(props) {
     return (
-    <section class="container mx-auto px-4 py-4 md:w-2/3 bg-neutral-600 my-4">
-        <h2 class="text-4xl font-bold uppercase text-neutral-300a pb-2">{props.headline}</h2>
-        <p class="text-neutral-300 font-mono text-lg">{props.blurb}</p>
+    <section className="container mx-auto px-4 py-4 md:w-1/2 rounded bg-neutral-600 my-4">
+        <h2 classNameName="text-4xl font-bold uppercase text-neutral-300a pb-2">{props.headline}</h2>
+        <p classNameName="text-neutral-300 font-mono text-lg">{props.blurb}</p>
         {props.form}
     </section>
     )
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,17 +1,41 @@
-function Form() {
+import React, { useState } from "react";
+
+const Form = () => {
+    const [status, setStatus] = useState("Submit");
+  const submitForm = async (e) => {
+    e.preventDefault();
+    setStatus("Sending...");
+    const { name, email, source } = e.target.elements;
+    let details = {
+      name: name.value,
+      email: email.value,
+      source: source.value,
+    };
+    let response = await fetch("http://localhost:5000/contact", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+      },
+      body: JSON.stringify(details),
+    });
+    setStatus("Submit");
+    let result = await response.json();
+    alert(result.status);
+  };
     return (
-        <div>
-            <h2>Get Free Quote</h2>
-            <p>Fill out this quick form and we will be in touch with how we can help</p>
-    <form>
-        <label for="name">Name:</label>
-        <input type="text" name="name"></input>
-        <label for="email">Email:</label>
-        <input type="email" name="email"></input>
-        <label for="source">How did you hear about us?</label>
-        <input type="text" name="source"></input>
+    <form onSubmit={submitForm}>
+        <label className="block text-gray-100 text-md font-bold mb-2 my-3" htmlFor="name">
+            Name:
+        </label>
+        <input className="shadow appearance-none text-center border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline;" type="text" id="name" name="name"></input>
+        <label className="block text-gray-100 text-md font-bold mb-2 my-3" htmlFor="email">Email:</label>
+        <input className="shadow appearance-none text-center border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline;" type="email" id="email" name="email"></input>
+        <label className="block text-gray-100 text-md font-bold mb-2 my-3" htmlFor="source">How did you hear about us?</label>
+        <input className="shadow appearance-none text-center border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline;" type="text" id="source" name="source"></input>
+        <button className="bg-yellow-500 hover:bg-yellow-700 text-grey-500 font-bold py-2 my-4 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
+        {status}
+        </button>
     </form>
-    </div>
     )
 }
 
